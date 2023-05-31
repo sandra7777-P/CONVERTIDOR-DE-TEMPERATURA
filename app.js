c3199c0c452a09b54e8b327d4ff3bcfd94cbaa5c
Sandra Piza <131202421+sandra7777-P@users.noreply.github.com>
2023-05-31 10:45:47 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// preguntar la temperatura en Celsius
+var temperature = parseInt(prompt("¿Cual es la temperatura en Celsius?"));
+//convertir celsius a fahrenheit
+var convertToFarenheit = (temperature*1.8) + 32;
+//mostrar resultado en la consola
+console.log("La temperatura en Fahrenheit es " + convertToFarenheit);
